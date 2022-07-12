15fbf9ff05e2370641b9a7fd562ec8e13079df13
박지수 Jisu Park <qkrwltn8976@dongguk.edu>
2022-07-12 20:19:07 +0900

    #1 Pure Redux: Counter

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import { createStore } from "redux"
+
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+number.innerText = 0;
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+// a function that modifies your data
+const countModifier = (count = 0, action) => {
+  // ... modify state
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+};
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+}
+
+// listen to changes on the store
+countStore.subscribe(onChange);
+
+// action must have a type
+const handleAdd = () => {
+  countStore.dispatch({ type: ADD })
+}
+
+const handleMinus = () => {
+  countStore.dispatch({ type: MINUS })
+}
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
+
