201fce287586f7af8ed9644a2cabb34740892d2d
박지수 Jisu Park <qkrwltn8976@dongguk.edu>
2022-07-13 21:05:09 +0900

    #1 Pure Redux: To Do List

--- src/index.js
+++ src/index.js
@@ -1,45 +1,76 @@
 import { createStore } from "redux"
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-number.innerText = 0;
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-const ADD = "ADD";
-const MINUS = "MINUS";
+const addToDo = (text) => {
+    return { type: ADD_TODO, text }
+}
 
-// a function that modifies your data
-const countModifier = (count = 0, action) => {
-  // ... modify state
-  switch (action.type) {
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
-    default:
-      return count;
-  }
-};
+const deleteToDo = (id) => {
+    return { type: DELETE_TODO, id }
+}
 
-const countStore = createStore(countModifier);
+const reducer = (state = [], action) => {
+    switch (action.type) {
+        case ADD_TODO:
+            const newTodoObj = { text: action.text, id: Date.now() };
+            return [newTodoObj, ...state]; // es6 spread
+        case DELETE_TODO:
+            const cleaned = state.filter(toDo => toDo.id !== action.id);
+            return cleaned; // filter: creates a new array with all elements that pass the test
+        default:
+            return state
+    }
+}
 
-const onChange = () => {
-  number.innerText = countStore.getState();
+// never mutate state -> return new state object
+// the only way that you can modify store is by sending action
+const store = createStore(reducer);
+
+const dispatchAddToDo = (text) => {
+    store.dispatch(addToDo(text));
+}
+
+const dispatchDeleteToDo = (e) => {
+    const id = parseInt(e.target.parentNode.id);
+    store.dispatch(deleteToDo(id));
+}
+
+// react repaint all stuff
+const paintTodos = () => {
+    const toDos = store.getState();
+    ul.innerHTML = "";
+
+    toDos.forEach(toDo => {
+        const li = document.createElement("li");
+        const btn = document.createElement("button");
+
+        btn.innerText = "DEL";
+        btn.addEventListener("click", dispatchDeleteToDo);
+
+        li.id = toDo.id;
+        li.innerText = toDo.text;
+
+        li.appendChild(btn);
+        ul.appendChild(li);
+    })
 }
 
 // listen to changes on the store
-countStore.subscribe(onChange);
+store.subscribe(paintTodos)
 
-// action must have a type
-const handleAdd = () => {
-  countStore.dispatch({ type: ADD })
+const onSubmit = e => {
+    e.preventDefault();
+
+    const toDo = input.value;
+    input.value = "";
+
+    dispatchAddToDo(toDo);
 }
 
-const handleMinus = () => {
-  countStore.dispatch({ type: MINUS })
-}
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
-
+form.addEventListener("submit", onSubmit);
