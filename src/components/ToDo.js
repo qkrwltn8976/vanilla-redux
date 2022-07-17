323ad9942c9d1444b9796624f6388edbef94bda7
박지수 Jisu Park <qkrwltn8976@dongguk.edu>
2022-07-17 18:48:09 +0900

    #4 REDUX TOOLKIT

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,21 +1,20 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { actionCreators } from "../store";
+import { remove } from "../store";
 
 function ToDo({ text, onBtnClick, id }) {
   return (
     <li>
-      <Link to={`/${id}`}>
-        {text} <button onClick={onBtnClick}>DEL</button>
-      </Link>
+      <Link to={`/${id}`}>{text}</Link>
+      <button onClick={onBtnClick}>DEL</button>
     </li>
   );
 }
 
 function mapStateToProps(dispatch, ownProps) {
   return {
-    onBtnClick: () => dispatch(actionCreators.deleteToDo(ownProps.id)),
+    onBtnClick: () => dispatch(remove(ownProps.id)),
   };
 }
 
--- src/store.js
+++ src/store.js
@@ -1,38 +1,50 @@
-import { createStore } from "redux";
+import {
+  configureStore,
+  //   createAction,
+  //   createReducer,
+  createSlice,
+} from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
+// createSlice
+const toDos = createSlice({
+  name: "toDosReducer",
+  initialState: [],
+  reducers: {
+    add: (state, action) => {
+      state.push({ text: action.payload, id: Date.now() }); // can mutate state, no use to return new state
+    },
+    remove: (state, action) =>
+      state.filter((toDo) => toDo.id !== action.payload), // should return state
+  },
+});
 
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
+const store = configureStore({ reducer: toDos.reducer }); // work with middleware (add defaults to the store)
 
-const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id: parseInt(id),
-  };
-};
-
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter((toDo) => toDo.id !== action.id);
-    default:
-      return state;
-  }
-};
-
-const store = createStore(reducer);
-
-export const actionCreators = {
-  addToDo,
-  deleteToDo,
-};
+export const { add, remove } = toDos.actions;
 
 export default store;
+
+// // REACT TOOLKIT
+// const addToDo = createAction("ADD");
+// const deleteToDo = createAction("DELETE");
+
+// const reducer = createReducer([], {
+//   // 1. return new state -> should return 2. mutate state -> immer & redux-toolkit do for you
+//   [addToDo]: (state, action) => {
+//     state.push({ text: action.payload, id: Date.now() }); // can mutate state, no use to return new state
+//   },
+//   [deleteToDo]: (state, action) =>
+//     state.filter((toDo) => toDo.id !== action.payload), // should return state
+// });
+
+// REACT REDUX
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case addToDo.type:
+//       return [{ text: action.payload, id: Date.now() }, ...state];
+//     case deleteToDo.type:
+//       return state.filter((toDo) => toDo.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
